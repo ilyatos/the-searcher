db170808075ef421397798329eff4d5a126634bc
Ilya Goryachev <ilyatos.gor@gmail.com>
2018-09-19 23:06:39 +0300

    getting connection parameters using config/db.php

---
+++ db_connetcion.php
@@ -0,0 +1,15 @@
+<?php
+//ob_start();
+
+try {
+    $dbConfig = require "config/db.php";
+
+    $connection = new PDO(
+        "mysql:dbname={$dbConfig['name']}; host={$dbConfig['host']}",
+        "{$dbConfig['username']}",
+        "{$dbConfig['password']}");
+
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
