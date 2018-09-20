d9ceb2f9207b244304481663a28657e3a583cddf
Ilya Goryachev <ilyatos.gor@gmail.com>
2018-09-20 21:35:30 +0300

    The Singleton-like class to get the instance of connection everywhere

--- classes/Connection.php
+++ classes/Connection.php
@@ -2,5 +2,32 @@
 
 
 class Connection {
+    /**
+     * @var PDO
+     */
+    private static $instance;
 
+    private function __construct() {
+        $dbConfig = include '../config/db.php';
+
+        self::$_instance = new PDO(
+            "mysql:dbname={$dbConfig['name']}; host={$dbConfig['host']}",
+            "{$dbConfig['username']}",
+            "{$dbConfig['password']}");
+
+        self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+    }
+
+    /**
+     * Get the instance of Connection class.
+     *
+     * @return Connection|PDO
+     */
+    public static function getInstance() {
+        if(self::$instance === null) {
+            self::$instance = new Connection();
+        }
+
+        return self::$instance;
+    }
 }
