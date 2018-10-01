1f00c98e278f78d606d5f8e9c0bd954fef432ba2
Ilya Goryachev <ilyatos.gor@gmail.com>
2018-10-01 23:31:18 +0300

    Guzzle is used

--- scripts/crawl.php
+++ scripts/crawl.php
@@ -5,10 +5,31 @@ require_once __DIR__ . '/../vendor/autoload.php';
 use Engine\CrawlerHelper;
 use Engine\DOMDocumentParser;
 use Engine\Models\Site;
+use GuzzleHttp\Client;
+
+$headers = [
+    "User-Agent: searcherBot/1.0\n"
+];
 
 function followLinks(string $url) {
+    global $headers;
+
+    $client = new Client([
+        'defaults' => [
+            'headers' => $headers,
+        ]
+    ]);
+
+    try {
+        $html = $client->request('GET', $url)->getBody()->getContents();
+    } catch (\GuzzleHttp\Exception\GuzzleException $e) {
+        Site::updateWhere(['broken' => 1], 'url', $url);
+        echo $e->getMessage();
+        return;
+    }
+
+    $parser = new DOMDocumentParser($html, $url);
     $helper = new CrawlerHelper($url);
-    $parser = new DOMDocumentParser($url);
 
     $links = $parser->getLinks();
     $helper->saveRawLinks($links);
@@ -19,11 +40,11 @@ function followLinks(string $url) {
     $seo = $parser->getSeoData();
     $helper->saveSeoData($seo);
 
-    unset($helper, $parser);
+    unset($client, $parser, $helper);
 }
 
 
-$url = 'https://dogsecrets.ru';
+$url = 'https://www.dogsecrets.ru';
 
 echo 'Crawling...' . PHP_EOL;
 $t = microtime(true);
